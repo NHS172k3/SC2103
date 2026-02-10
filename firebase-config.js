e71adf7074b7cbe9f70b8f8e9e6cd69fbcebf3e2
NHS172k3 <harinachiappansubramanian@gmail.com>
2026-02-11 03:25:00 +0800

    Add Firebase config for GitHub Pages deployment

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration
+// This file contains your actual Firebase credentials
+// It's in .gitignore so it won't be committed to git
+
+const firebaseConfig = {
+apiKey: "AIzaSyAujNIOaFRr62TAK-g5HDU-V8fB4lQ2ZpU",
+authDomain: "sc2103-lab2-quiz.firebaseapp.com",
+projectId: "sc2103-lab2-quiz",
+storageBucket: "sc2103-lab2-quiz.firebasestorage.app",
+messagingSenderId: "639799985384",
+appId: "1:639799985384:web:131a6e21d1630a8f96355d"
+};
+
+const appId = "lab2-quiz-2026";
